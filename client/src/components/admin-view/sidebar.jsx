9d43a5b7b09ac24d961d7f3aeecc96ee91730c63
Bhavya Suthar <bhavyasuthar06@gmail.com>
2025-02-14 13:49:02 +0530

    Admin sidebar design

--- client/src/components/admin-view/sidebar.jsx
+++ client/src/components/admin-view/sidebar.jsx
@@ -1,7 +1,76 @@
-function AdminSidebar() {
-    return ( 
-        <div>Admin Sidebar</div>
-     );
+import { ChartNoAxesCombined } from "lucide-react";
+import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { BadgeCheck, LayoutDashboard, ShoppingBasket } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "../ui/sheet";
+
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icons: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icons: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icons: <BadgeCheck />,
+  },
+];
+
+function MenuItems({setOpen}) {
+  const navigate = useNavigate();
+  return (
+    <nav className="mt-8 flex-col flex gap-2">
+      {adminSidebarMenuItems.map((menuItems) => (
+        <div
+          className="flex items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground text-xl cursor-pointer"
+          key={menuItems.id}
+          onClick={() => {navigate(menuItems.path)
+          setOpen ? setOpen(false)  : null }}
+        >
+          {menuItems.icons}
+          <span>{menuItems.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
+}
+function AdminSidebar({open, setOpen}) {
+  return (
+    <Fragment>
+    <Sheet open={open} onOpenChange={setOpen}>
+        <SheetContent side="left" className="w-64">
+            <div className="flex flex-col h-full">
+                <SheetHeader className="border-b">
+                    <SheetTitle className="flex gap-2 mt-5 mb-5">
+                        <ChartNoAxesCombined size={30} />
+                        <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+                    </SheetTitle>
+                </SheetHeader>
+                <MenuItems setOpen={setOpen}/>
+            </div>
+        </SheetContent>
+    </Sheet>
+      <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+        <div
+          className="flex items-center gap-2 cursor-pointer"
+          onClick={() => navigate("/admin/dashboard")}
+        >
+          <ChartNoAxesCombined size={30} />
+          <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+        </div>
+        <MenuItems />
+      </aside>
+    </Fragment>
+  );
 }
 
 export default AdminSidebar;
--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,39 +1,42 @@
+
+
 export const regiterFormControls = [
-    {
-        name: "userName",
-        label: "User Name",
-        placeholder: "Enter Your User Name",
-        componentType: "input",
-        type: "text",
-    },
-    {
-        name: "email",
-        label: "Email",
-        placeholder: "Enter Your Email",
-        componentType: "input",
-        type: "email",
-    },
-    {
-        name: "password",
-        label: "Password",
-        placeholder: "Enter Your Password",
-        componentType: "input",
-        type: "password",
-    }
-]
+  {
+    name: "userName",
+    label: "User Name",
+    placeholder: "Enter Your User Name",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter Your Email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter Your Password",
+    componentType: "input",
+    type: "password",
+  },
+];
 export const loginFormControls = [
-    {
-        name: "email",
-        label: "Email",
-        placeholder: "Enter Your Email",
-        componentType: "input",
-        type: "email",
-    },
-    {
-        name: "password",
-        label: "Password",
-        placeholder: "Enter Your Password",
-        componentType: "input",
-        type: "password",
-    }
-]
+  {
+    name: "email",
+    label: "Email",
+    placeholder: "Enter Your Email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    placeholder: "Enter Your Password",
+    componentType: "input",
+    type: "password",
+  },
+];
+ 
