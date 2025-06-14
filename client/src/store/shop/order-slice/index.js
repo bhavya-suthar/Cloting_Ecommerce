545a72d1ecda20298136d148cdd069df8141be80
Bhavya Suthar <bhavyasuthar06@gmail.com>
2025-06-14 22:40:42 +0530

    create order slice

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -1,17 +1,38 @@
-const { createSlice } = require("@reduxjs/toolkit")
+import axios from "axios"
+
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit")
 
 
 const initialState = {
     approvalURL:null,
     isLoading:false,
-    order:null
+    orderId:null
 }
+
+export const createNewOrder = createAsyncThunk('/order/createNewOrder',async(orderData)=>{
+    const response = await axios.post("http://localhost:5000/api/shop/order/create",orderData)
+    return response.data
+})
 
 const shoppingOrderSlice = createSlice({
     name:'shoppingOrderSlice',
     initialState,
     reducers:{},
-    extraReducers:(builder)=>{}
+    extraReducers:(builder)=>{
+        builder.addCase(createNewOrder.pending,(state)=>{
+            state.isLoading = true
+        })
+        .addCase(createNewOrder.fulfilled,(state,action)=>{
+            state.isLoading = false,
+            state.approvalURL = action.payload.approvalURL
+            state.orderId = action.payload.orderId
+        })
+        .addCase(createNewOrder.rejected,(state)=>{
+            state.isLoading = false,
+            state.approvalURL =null,
+            state.orderId = null
+        })
+    }
 })
 
 export default shoppingOrderSlice.reducer
